ffce88816a768d25fae674a7e8e4d2708c8e1fbd
Damian Palpacelli <damian.palpacelli@gmail.com>
2014-02-18 15:25:33 -0300

    added product controller

---
+++ app/scripts/controllers/product.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('fampFrontendApp')
+  .controller('ProductCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
